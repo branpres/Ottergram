03972ef6aaf14745c7863395ac5456e64c5a8949
Brandon Preston <brandon.preston@gmail.com>
2023-12-20 17:16:32 -0500

    Added footer component.

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <footer className="footer-component">
+            <p>Built by: <i>Brandon Preston</i> <a href="https://github.com/branpres/Ottergram">GitHub</a></p>
+        </footer>
+    )
+}
+
+export default Footer;
